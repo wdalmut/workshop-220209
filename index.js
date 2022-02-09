8fba99c8da57892eb48727c0e689af57c788ecb7
Walter Dal Mut <walter.dalmut@gmail.com>
2022-02-09 10:35:59 +0000

    featuer a

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}!!!!!!!!!!!!!!`)
 })
